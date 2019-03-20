c974f6dab9c85df2dbe32ed49c7ad958d78f9e55
Ebenezer Ennim <ebenezerennim@Ebenezers-MacBook-Pro.local>
2019-03-20 22:50:25 +0100

    [chore #164779291]create travis integration

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Role Assigner!');
+  });
+
 app.listen(port);
 
 export default app;
