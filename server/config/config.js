8ad97091c7eefe986c683cfdf4cf62d39c1193f4
Ebenezer Ennim <ebenezerennim@Ebenezers-MacBook-Pro.local>
2019-03-21 10:06:12 +0100

    [chore #164779403]create configuration for sequelize and postgresql

---
+++ server/config/config.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOSTNAME,
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+    },
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+    },
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+    },
+  },
+};
